984e41a6d637eba13058804e0f51879524c5f568
Seungjoon Ahn <93236696+joon0110@users.noreply.github.com>
2022-05-19 02:11:20 -0400

    Add files via upload

---
+++ GoTsearchsetup/config.py
@@ -0,0 +1,6 @@
+
+neo_url = "neo4j+s://c371fabb.databases.neo4j.io:7687"
+neo_auth = ("neo4j", "7TDVvpX9uYPpZ8PZtVySIC6l-p58uImqTkX-4d-PHHU")
+
+mongodb_url = \
+    "mongodb://sa3883:Dkstmdwns011001@cluster0-shard-00-00.2xeqe.mongodb.net:27017,cluster0-shard-00-01.2xeqe.mongodb.net:27017,cluster0-shard-00-02.2xeqe.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xl16ms-shard-0&authSource=admin&retryWrites=true&w=majority"
